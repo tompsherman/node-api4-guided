9cf882839cd6c2e7b411f52c628df26e1fbfbf1e
unknown <tompsherman@gmail.com>
2020-10-29 12:58:53 -0400

    ready for deployment

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const server = require("./api/server.js");
+require('dotenv').config()
 
-const port = 5000;
+const server = require("./api/server.js")
+
+const port = process.env.PORT || 5000
+const secret = process.env.SECRET || "the secret for development"
+
+console.log(secret, port);
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
